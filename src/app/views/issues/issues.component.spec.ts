276a9bea237a905491f23cef4159c2d6205c4111
akosimarksalvador <markanthonybuhaysalvador@gmail.com>
2020-09-25 20:37:24 +0800

    .specs changes on new components (commits, issues, read-me)

--- src/app/views/issues/issues.component.spec.ts
+++ src/app/views/issues/issues.component.spec.ts
@@ -1,4 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Title } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { GithubService } from 'src/app/api/base/gifthub/github.service';
+import { AppComponent } from 'src/app/app.component';
+import { ConfigService } from 'src/app/app.config';
+import { BaseComponent } from 'src/app/base/base.component';
+import { LayoutComponent } from 'src/app/layout/layout.component';
+import { LoadingService } from 'src/app/libraries/loading.service';
+import { MessageService } from 'src/app/libraries/message.service';
+import { StorageService } from 'src/app/libraries/storage.service';
+import { StringService } from 'src/app/libraries/string.service';
+import { RepositoriesComponent } from '../repositories/repositories.component';
 
 import { IssuesComponent } from './issues.component';
 
@@ -8,9 +25,30 @@ describe('IssuesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IssuesComponent ]
+      imports: [
+        FormsModule,
+        RouterTestingModule.withRoutes([
+          { path: '', component: LayoutComponent },
+        ]),
+        HttpClientTestingModule,
+        NgxSpinnerModule,
+        MatSnackBarModule,
+        FormsModule,
+        ReactiveFormsModule],
+      declarations: [AppComponent, LayoutComponent, BaseComponent, RepositoriesComponent, IssuesComponent],
+      providers: [
+        Title,
+        FormBuilder,
+        ConfigService,
+        StorageService,
+        StringService,
+        LoadingService,
+        GithubService,
+        MessageService,
+        HttpClient,
+        LayoutComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/views/read-me/read-me.component.spec.ts
+++ src/app/views/read-me/read-me.component.spec.ts
@@ -1,4 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Title } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { GithubService } from 'src/app/api/base/gifthub/github.service';
+import { AppComponent } from 'src/app/app.component';
+import { ConfigService } from 'src/app/app.config';
+import { BaseComponent } from 'src/app/base/base.component';
+import { LayoutComponent } from 'src/app/layout/layout.component';
+import { LoadingService } from 'src/app/libraries/loading.service';
+import { MessageService } from 'src/app/libraries/message.service';
+import { StorageService } from 'src/app/libraries/storage.service';
+import { StringService } from 'src/app/libraries/string.service';
+import { RepositoriesComponent } from '../repositories/repositories.component';
 
 import { ReadMeComponent } from './read-me.component';
 
@@ -6,12 +23,35 @@ describe('ReadMeComponent', () => {
   let component: ReadMeComponent;
   let fixture: ComponentFixture<ReadMeComponent>;
 
+ 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReadMeComponent ]
+      imports: [
+        FormsModule,
+        RouterTestingModule.withRoutes([
+          { path: '', component: LayoutComponent },
+        ]),
+        HttpClientTestingModule,
+        NgxSpinnerModule,
+        MatSnackBarModule,
+        FormsModule,
+        ReactiveFormsModule],
+      declarations: [AppComponent, LayoutComponent, BaseComponent, RepositoriesComponent, ReadMeComponent],
+      providers: [
+        Title,
+        FormBuilder,
+        ConfigService,
+        StorageService,
+        StringService,
+        LoadingService,
+        GithubService,
+        MessageService,
+        HttpClient,
+        LayoutComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ReadMeComponent);
